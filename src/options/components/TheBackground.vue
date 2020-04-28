ffc1332c060eb3c05c77a35bf53e2b51505227e0
Go Noji <gisosyadfe@gmail.com>
2020-04-29 05:20:44 +0900

    [update] 背景グラデーションを 3 重にし、色の合成を変更

--- src/options/components/TheBackground.vue
+++ src/options/components/TheBackground.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="background">
+  <div
+    class="background"
+    :style="'background-color: '+color"
+  >
     <div
-      class="backgroundBack"
-      :style="'background-color: '+colorSetting.front"
+      :style="'background:linear-gradient('+Math.floor(Math.random() * 360)+'deg, '+getKindColorCode(color, Math.floor(Math.random() * 60), 70, 0)+' 10%, '+getKindColorCode(color, 180, 0 , 0)+')'"
+      class="backgroundFront"
     ></div>
     <div
+      :style="'background: linear-gradient('+Math.floor(Math.random() * 360)+'deg, '+getKindColorCode(color, Math.floor(Math.random() * 60), -40, -70)+' 70%, '+getKindColorCode(color, 180, 0 , 0)+')'"
       class="backgroundFront"
-      :style="'background: linear-gradient('+colorSetting.deg+', '+colorSetting.back+' 10%, '+colorSetting.front+')'"
+    ></div>
+    <div
+      class="backgroundBack"
+      :style="'background-color: '+color"
     ></div>
   </div>
 </template>
@@ -16,6 +23,95 @@
   import {USE_PRESET_KEY} from "@/options/compositions/presetComposition";
   import {isColorKeys} from "@/settings/interface";
   import {COLORS} from "@/settings/settings";
+
+  /**
+   * rgb 形式のカラーコードを hsl 形式に変換して返す
+   * @param red
+   * @param green
+   * @param blue
+   */
+  const getHSL = (red: number, green: number, blue: number): {hue: number, saturation: number, lightness: number} => {
+    //三食のうち一つでも 0 ~ 255 の範囲から外れていたら 0 0 0 を返す
+    if ([red, green, blue].some(value => value > 255 || 0 > value)) {
+      return {hue: 0, saturation: 0, lightness: 0};
+    }
+
+    //各色の最大値と最小値を求める
+    const max = Math.max(red, green, blue);
+    const min = Math.min(red, green, blue);
+
+    //輝度を求める
+    const lightness = Math.round((max + min) / 2 / 255 * 100);
+
+    //彩度を求める
+    const saturation = (max + min) / 2 > 217
+      ? Math.floor((max - min) / (510 - max - min) * 100)
+      : Math.floor((max - min) / (max + min) * 100);
+
+    //色相を求める
+    //無彩色
+    let hue = 0;
+
+    //赤系
+    if (red > green && red > blue) {
+      hue = 60 * ((green - blue) / (max - min));
+    }
+
+    //緑系
+    else if (green > red && green > blue) {
+      hue = 60 * ((blue - red) / (max - min)) + 120;
+    }
+
+    //青系
+    else if (blue > red && blue > green) {
+      hue = 60 * ((red - green) / (max - min)) + 240;
+    }
+
+    //返す
+    return {hue, saturation, lightness};
+  };
+
+  /**
+   * hsl 形式のカラーコードを rba 形式に変換して返す
+   * @param hue
+   * @param saturation
+   * @param lightness
+   */
+  const getRGB = (hue: number, saturation: number, lightness: number): {red: number, green: number, blue: number} => {
+    //彩度 or 輝度 が 0 ~ 100 の範囲外だったら 0 0 0 を返す
+    if (saturation < 0 || 100 < saturation || lightness < 0 || 100 < lightness) {
+      return {red: 0, green: 0, blue: 0};
+    }
+
+    //hue の範囲を 0 ~ 359 へ補正する
+    hue = Math.round(hue - (Math.floor(hue / 360) * 360));
+
+    //rgb の最大値・最小値を求める
+    const max = lightness < 50
+      ? Math.round(2.55 * (lightness + lightness * (saturation / 100)))
+      : Math.round(2.55 * (lightness + (100 - lightness) * (saturation / 100)));
+    const min = lightness < 50
+      ? Math.round(2.55 * (lightness - lightness * (saturation / 100)))
+      : Math.round(2.55 * (lightness - (100 - lightness) * (saturation / 100)));
+
+    //60 刻みで赤緑青の計算方法を分ける
+    if (0 <= hue && hue <= 60) {
+      return {red: max, green: Math.round((hue / 60) * (max - min) + min), blue: min};
+    }
+    else if (61 <= hue && hue <= 120) {
+      return {red: Math.round(((120 - hue) / 60) * (max - min) + min), green: max, blue: min};
+    }
+    else if (121 <= hue && hue <= 180) {
+      return {red: min, green: max, blue: Math.round(((hue - 120) / 60) * (max - min) + min)};
+    }
+    else if (181 <= hue && hue <= 240) {
+      return {red: min, green: Math.round(((240 - hue) / 60) * (max - min) + min), blue: max};
+    }
+    else if (241 <= hue && hue <= 300) {
+      return {red: Math.round(((hue - 240) / 60) * (max - min) + min), green: min, blue: max};
+    }
+    return {red: max, green: min, blue: Math.round(((360 - hue) / 60) * (max - min) + min)};
+  };
 
   export default createComponent({
     setup(_, context: SetupContext) {
@@ -27,97 +123,35 @@
         return;
       }
 
-      /**
-       * rgb 形式のカラーコードを hsl 形式に変換して返す
-       * @param red
-       * @param green
-       * @param blue
-       */
-      const getHSL = (red: number, green: number, blue: number): {hue: number, saturation: number, lightness: number} => {
-        //三食のうち一つでも 0 ~ 255 の範囲から外れていたら 0 0 0 を返す
-        if ([red, green, blue].some(value => value > 255 || 0 > value)) {
-          return {hue: 0, saturation: 0, lightness: 0};
+      //現在表示すべき色を返す
+      const color = computed<string>(() => {
+        //設定画面は特別な背景色を返す
+        if (context.root.$route.path.match(/^.*\/setting$/)) {
+          return '#263238';
         }
 
-        //各色の最大値と最小値を求める
-        const max = Math.max(red, green, blue);
-        const min = Math.min(red, green, blue);
+        //プリセットの対象インデックス
+        const index = Number(context.root.$route.params['index'] === undefined ? -1 : context.root.$route.params['index']);
 
-        //輝度を求める
-        const lightness = Math.round((max + min) / 2 / 255 * 100);
-
-        //彩度を求める
-        const saturation = (max + min) / 2 > 217
-          ? Math.floor((max - min) / (510 - max - min) * 100)
-          : Math.floor((max - min) / (max + min) * 100);
-
-        //色相を求める
-        //無彩色
-        let hue = 0;
-
-        //赤系
-        if (red > green && red > blue) {
-          hue = 60 * ((green - blue) / (max - min));
+        //プリセットが見つからなかったらデフォルト色を返す
+        if (index === -1 || store.state.presets[index] === undefined) {
+          return '#9E9E9E';
         }
 
-        //緑系
-        else if (green > red && green > blue) {
-          hue = 60 * ((blue - red) / (max - min)) + 120;
+        //現在のプリセットが示す色名を取得
+        const color = store.state.presets[index].color;
+
+        //色名が以上ならデフォルト色を返す
+        if ( ! isColorKeys(color) || typeof color !== 'string') {
+          return '#9E9E9E';
         }
 
-        //青系
-        else if (blue > red && blue > green) {
-          hue = 60 * ((red - green) / (max - min)) + 240;
-        }
-
-        //返す
-        return {hue, saturation, lightness};
-      };
-
-      /**
-       * hsl 形式のカラーコードを rba 形式に変換して返す
-       * @param hue
-       * @param saturation
-       * @param lightness
-       */
-      const getRGB = (hue: number, saturation: number, lightness: number): {red: number, green: number, blue: number} => {
-        //彩度 or 輝度 が 0 ~ 100 の範囲外だったら 0 0 0 を返す
-        if (saturation < 0 || 100 < saturation || lightness < 0 || 100 < lightness) {
-          return {red: 0, green: 0, blue: 0};
-        }
-
-        //hue の範囲を 0 ~ 359 へ補正する
-        hue = Math.round(hue - (Math.floor(hue / 360) * 360));
-
-        //rgb の最大値・最小値を求める
-        const max = lightness < 50
-          ? Math.round(2.55 * (lightness + lightness * (saturation / 100)))
-          : Math.round(2.55 * (lightness + (100 - lightness) * (saturation / 100)));
-        const min = lightness < 50
-          ? Math.round(2.55 * (lightness - lightness * (saturation / 100)))
-          : Math.round(2.55 * (lightness - (100 - lightness) * (saturation / 100)));
-
-        //60 刻みで赤緑青の計算方法を分ける
-        if (0 <= hue && hue <= 60) {
-          return {red: max, green: Math.round((hue / 60) * (max - min) + min), blue: min};
-        }
-        else if (61 <= hue && hue <= 120) {
-          return {red: Math.round(((120 - hue) / 60) * (max - min) + min), green: max, blue: min};
-        }
-        else if (121 <= hue && hue <= 180) {
-          return {red: min, green: max, blue: Math.round(((hue - 120) / 60) * (max - min) + min)};
-        }
-        else if (181 <= hue && hue <= 240) {
-          return {red: min, green: Math.round(((240 - hue) / 60) * (max - min) + min), blue: max};
-        }
-        else if (241 <= hue && hue <= 300) {
-          return {red: Math.round(((hue - 240) / 60) * (max - min) + min), green: min, blue: max};
-        }
-        return {red: max, green: min, blue: Math.round(((360 - hue) / 60) * (max - min) + min)};
-      };
+        //プリセットに設定されている色を返す
+        return String(COLORS[color]);
+      });
 
       //16 進数カラーコードを受け取り、hls で h に +120, s に -30, l に +30 した色を 16 進数カラーコードで返す
-      const getKindColorCode = (code: string): string => {
+      const getKindColorCode = (code: string, hue: number, saturation: number, lightness: number): string => {
         //code から # を削除し、小文字化
         code = code.replace('#', '').toLowerCase();
 
@@ -138,54 +172,15 @@
         const hsl = getHSL(parseInt(code.substr(0, 2), 16), parseInt(code.substr(2, 2), 16), parseInt(code.substr(4, 2), 16));
 
         //ずらした色を返す
-        const rgb = getRGB(hsl.hue + 120, hsl.saturation < 30 ? 0 : hsl.saturation - 30, hsl.lightness > 70 ? 100 : hsl.lightness + 30);
+        const rgb = getRGB(
+          hsl.hue + hue,
+          saturation < 0 ? Math.max(hsl.saturation + saturation, 0) : Math.min(hsl.saturation + saturation, 100),
+          lightness < 0 ? Math.max(hsl.lightness + lightness, 0) : Math.min(hsl.lightness + lightness, 100)
+        );
 
         //合成して返す
         return '#'+rgb.red.toString(16).padStart(2, '0')+rgb.green.toString(16).padStart(2, '0')+rgb.blue.toString(16).padStart(2, '0');
       };
-
-      //現在表示すべき色を返す
-      const colorSetting = computed(() => {
-        //設定画面は特別な背景色を返す
-        if (context.root.$route.path.match(/^.*\/setting$/)) {
-          return {
-            front: '#263238',
-            back: getKindColorCode('#263238'),
-            deg: String(Math.random() * 361)+'deg'
-          };
-        }
-
-        //プリセットの対象インデックス
-        const index = Number(context.root.$route.params['index'] === undefined ? -1 : context.root.$route.params['index']);
-
-        //プリセットが見つからなかったらデフォルト色を返す
-        if (index === -1 || store.state.presets[index] === undefined) {
-          return {
-            front: '#9E9E9E',
-            back: getKindColorCode('#9E9E9E'),
-            deg: String(Math.random() * 361)+'deg'
-          };
-        }
-
-        //現在のプリセットが示す色名を取得
-        const color = store.state.presets[index].color;
-
-        //色名が以上ならデフォルト色を返す
-        if ( ! isColorKeys(color) || typeof color !== 'string') {
-          return {
-            front: '#9E9E9E',
-            back: getKindColorCode('#9E9E9E'),
-            deg: String(Math.random() * 361)+'deg'
-          };
-        }
-
-        //プリセットに設定されている色を返す
-        return {
-          front: COLORS[color],
-          back: getKindColorCode(String(COLORS[color])),
-          deg: String(Math.random() * 361)+'deg'
-        };
-      });
 
       //データを読み込む
       onMounted(() => {
@@ -193,7 +188,7 @@
       });
 
       //データの伝播
-      return {colorSetting};
+      return {color, getKindColorCode};
     }
   });
 </script>
@@ -205,6 +200,7 @@
     left: 0;
     width: 100%;
     height: 100vh;
+    transition: background-color .2s ease-out;
   }
   .backgroundBack{
     position: absolute;
@@ -213,6 +209,8 @@
     width: 100%;
     height: 100vh;
     transition: background-color .2s ease-out;
+    opacity: .5;
+    mix-blend-mode: overlay;
   }
   .backgroundFront{
     position: absolute;
@@ -220,6 +218,8 @@
     left: 0;
     width: 100%;
     height: 100vh;
+    transition: background-color .2s ease-out;
     opacity: .5;
+    mix-blend-mode: difference;
   }
 </style>
